dd1cbb9cb29c9e528da347e1eaa72ecfdba4a980
Owen Robison <orobison@proton.me>
2025-02-04 10:30:16 -0500

    simple functions

---
+++ script.js
@@ -0,0 +1,36 @@
+let firstNumber = undefined;
+let secondNumber = undefined;
+let operator = undefined;
+
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate(a, b, op) {
+    switch (op) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
